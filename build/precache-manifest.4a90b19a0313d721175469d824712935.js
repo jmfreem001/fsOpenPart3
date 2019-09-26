99d0141229e4ccc8271d727d4061c2cb8f2d9ad1
Josh Freeman <jmfreem001@gmail.com>
2019-09-25 22:11:46 -0500

    Add frontend build

---
+++ build/precache-manifest.4a90b19a0313d721175469d824712935.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04fdd901759e2fa20a3ae6f34d26e93c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5b367bbef8efc5b6113",
+    "url": "/static/css/main.c62829e1.chunk.css"
+  },
+  {
+    "revision": "22d07d30d3961d99ba8f",
+    "url": "/static/js/2.7df1df7d.chunk.js"
+  },
+  {
+    "revision": "d5b367bbef8efc5b6113",
+    "url": "/static/js/main.23a6c944.chunk.js"
+  },
+  {
+    "revision": "0a3909685390b3afae4a",
+    "url": "/static/js/runtime~main.ce00a3d8.js"
+  }
+]);
